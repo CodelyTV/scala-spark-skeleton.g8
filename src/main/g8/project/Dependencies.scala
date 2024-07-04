73c7f483cae23c59a751ce87f833ced067ff3a71
Christian Herrera <christian.herrera@adevinta.com>
2024-07-04 13:36:54 +0200

    Add essential spark dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,10 +2,12 @@ import sbt._
 
 object Dependencies {
   private val prod = Seq(
-    "com.github.nscala-time" %% "nscala-time" % "$nscala-time_version$",
-    "com.lihaoyi"            %% "pprint"      % "$pprint_version$"
+    "com.github.nscala-time" %% "nscala-time"          % "$nscala-time_version$",
+    "com.lihaoyi"            %% "pprint"               % "$pprint_version$",
+    "org.apache.spark"       %% "spark-core"           % "$spark_version$" % Provided,
+    "org.apache.spark"       %% "spark-sql"            % "$spark_version$" % Provided,
+    "org.apache.spark"       %% "spark-streaming"      % "$spark_version$" % Provided
   )
-
   private val test = Seq(
     "org.scalatest" %% "scalatest" % "$scalatest_version$",
     "org.scalamock" %% "scalamock" % "$scalamock_version$"
